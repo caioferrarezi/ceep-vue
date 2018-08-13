4745d4356a6fb9df728e369abcec7a20461eca0d
Caio Ferrarezi <caioferrarezi2@gmail.com>
2018-08-13 00:30:31 -0300

    ajuste de funcionalidades, agora a busca redireciona para a pagina de resultados com uma query

--- src/components/Painel.vue
+++ src/components/Painel.vue
@@ -1,22 +1,12 @@
 <template>
   <div class="hello">
+    <Nav @search="search"></Nav>
     <div class="container">
-     <div class="row justify-content-center">
-
-      <div class="col-12 d-flex align-items-center justify-content-around mb-5">
-        <h1>{{ msg }}</h1>
-        <button v-if="newCard == false" class="btn btn-primary" @click.prevent="addNew">Add new card</button>
-        <button v-else class="btn btn-danger" @click.prevent="cancel">Cancel</button>
+      <div class="row justify-content-center mb-4">
+        <h1 class="sr-only">{{ msg }}</h1>
       </div>
-
-      <div v-if="newCard == true" class="col-12 col-md-8 col-md-6 col-lg-4 mb-5">
-        <FormNewCard></FormNewCard>
-      </div>
-      
-     </div>
-
-      <div class="row justify-content-center">
-        <div class="col-12 col-md-8 col-lg-4" v-for="(card, i) in cards" :key="i">
+      <div class="row justify-content-start">
+        <div class="col-12 col-md-8 col-lg-4 my-3" v-for="(card, i) in cards" :key="i">
           <Card :config="card"></Card>
         </div>
       </div>
@@ -25,40 +15,41 @@
 </template>
 
 <script>
-import FormNewCard from '@/components/FormNewCard'
-import Card from '@/components/Card'
-import { db } from '../main'
+import Nav from "@/components/Nav";
+import Card from "@/components/Card";
+import { db } from "../main";
 
 export default {
-  name: 'Painel',
-  data () {
+  name: "Painel",
+  data() {
     return {
-      msg: 'Welcome to Ceep',
+      msg: "Welcome to Ceep",
       cards: [],
-      newCard: false,
-    }
+      auxCards: [],
+    };
   },
   components: {
-    FormNewCard,
-    Card,
+    Nav,
+    Card
   },
   methods: {
-    addNew () {
-      this.newCard = true;
-    },
-    cancel () {
-      this.newCard = false;
+    search: function(src) {
+      this.$router.replace({
+        path: 'search',
+        query: {
+          key: src.key
+        }
+      });
     }
   },
-  firestore () {
+  firestore() {
     return {
-      cards: db.collection('cards').orderBy('createdAt', 'desc')
-    }
+      cards: db.collection("cards").orderBy("createdAt", "desc")
+    };
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style>
-
 </style>
