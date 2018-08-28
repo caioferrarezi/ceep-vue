b743d8d22bd78002a40a611829724eb6d11f3620
Caio Ferrarezi <caioferrarezi2@gmail.com>
2018-08-28 01:29:36 -0300

    novo formulario de edição do card em modal

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,48 +1,59 @@
 <template>
-    <article class="card bg-light text-left">
-      
-        <template v-if="editable == false && config.firstTime == false">
+    <div class="card-wrapper" @click="cancelEdit(config.id)">
+      <template>
+        <article class="card bg-light text-left" @click.stop>
           <div class="card-body">
             <span class="badge text-light" :class="[config.badge.class]">{{ config.badge.label | capitalize }}</span>
             <h2 class="h4 card-title mt-1">{{ config.title }}</h2>
             <p class="card-text">{{ config.text }}</p>
           </div>
-          <div class="card-header text-right">
+          <div class="card-footer text-right">
             <button class="btn btn-primary" @click="editCard()">Edit card</button>
           </div>
-        </template>
+        </article>
+      </template>
 
-        <template v-else>
-          <form @submit.prevent="saveChanges(config.id)" @input="isChanged = true">
-            <div class="card-body">
+      <transition name="show-card">
+        <template v-if="editable == true || config.firstTime == true">
+          <div class="card-modal">
+            <div class="card bg-light text-left" @click.stop>
+              <form @submit.prevent="saveChanges(config.id)" @input="isChanged = true">
+                <div class="card-body">
 
-              <div class="form-group">
-                <div class="d-inline-block custom-radio mr-2" v-for="badge in badges" :key="badge.label">
-                  <input :id="badge.label" type="radio" v-model="cat" :value="badge.label">
-                  <label :for="badge.label" class="badge text-light" :class="badge.class">{{ badge.label | capitalize }}</label>
+                  <div class="form-group">
+                    <div class="d-inline-block custom-radio mr-2" v-for="badge in badges" :key="badge.label">
+                      <input :id="badge.label" type="radio" v-model="cat" :value="badge.label">
+                      <label :for="badge.label" class="badge text-light" :class="badge.class">{{ badge.label | capitalize }}</label>
+                    </div>
+                  </div>
+
+                  <div class="form-group h4">
+                    <span class="sr-only"><label for="title sr-only">Title</label></span>
+                    <input type="text" class="form-control form-control-custom" id="title" v-model="config.title">
+                  </div>
+
+                  <div class="form-group">
+                    <span class="sr-only"><label for="desc">Description</label></span>
+                    <textarea v-model="config.text" id='desc' class="form-control form-control-custom"></textarea>
+                  </div>
+
                 </div>
-              </div>
 
-              <div class="form-group">
-                <label for="title">Title</label>
-                <input v-validate="required" type="text" class="form-control" id="title" v-model="config.title">
-              </div>
-
-              <div class="form-group">
-                <label for="desc">Description</label>
-                <textarea v-model="config.text" id='desc' class="form-control"></textarea>
-              </div>
-
+                <div class="card-footer d-flex justify-content-between">
+                  <div class="col-auto p-0">
+                    <button class="btn btn-link text-danger" @click.prevent="removeCard(config.id)">Delete</button>
+                  </div>
+                  <div class="col-auto p-0">
+                    <button class="btn btn-link" @click.prevent="cancelEdit()">Cancel</button>
+                    <button class="btn btn-primary" type="submit">Save card</button>
+                  </div>
+                </div>
+              </form>
             </div>
-
-            <div class="card-footer text-right">
-              <button class="btn btn-link" @click="removeCard(config.id)">Delete</button>
-              <button class="btn btn-primary" type="submit">Save card</button>
-            </div>
-          </form>
+          </div>
         </template>
-
-    </article>
+      </transition>
+    </div>
 </template>
 
 <script>
@@ -58,13 +69,15 @@ export default {
   data() {
     return {
       editable: false,
-      badges: [],
+      title: '',
+      text: '',
       cat: '',
     };
   },
   methods: {
     removeCard (id) {
       db.collection("cards").doc(id).delete();
+      this.editable = false;
     },
     saveChanges (id) {
       db.collection('cards').doc(id).update({
@@ -72,13 +85,19 @@ export default {
         title: this.config.title,
         text: this.config.text,
         badge: this.computedBadge[0],
+      }).then(docRef => {
+        alert('Card succesfully written')
+      }).catch(error => {
+        console.log(error);
       });
-
       this.editable = false;
     },
     editCard () {
       this.editable = true;
     },
+    cancelEdit () {
+      this.editable = false;
+    }
   },
   mounted () {
     this.cat = this.config.badge.label;
@@ -102,6 +121,26 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style>
 
+.card-modal {
+  background-color: rgba(0, 0, 0, 0.6);
+  position: fixed;
+  top: 0;left: 0;right: 0;bottom: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  z-index: 9999;
+}
+.card-modal .card {
+  width: 100%;
+  max-width: 576px;
+}
+
+.form-control.form-control-custom {
+  background-color: transparent;
+  border-color: transparent;
+  font-size: inherit;
+}
+
 .custom-radio {
     position: relative;
 }
@@ -117,6 +156,22 @@ export default {
 .custom-radio input:checked ~ .badge,
 .custom-radio .badge:hover {
     box-shadow: inset 0 0 0 20px rgba(0, 0, 0, 0.3);
+}
+
+.show-card-enter-active {
+  animation: showCard 0.3s ease;
+}
+.show-card-leave-active {
+  animation: showCard 0.3s ease reverse;
+}
+
+@keyframes showCard {
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
 }
 
 </style>
