5e77119a7223e3010a3ba81bf2cb32a144fa6be0
Caio Ferrarezi <caioferrarezi2@gmail.com>
2018-08-30 17:41:32 -0300

    Estilos e animções gerais adicionados

--- src/App.vue
+++ src/App.vue
@@ -18,4 +18,59 @@ export default {
   text-align: center;
   color: #2c3e50;
 }
+
+.load-modal {
+  position: absolute;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: rgba(255, 255, 255, 0.8);
+}
+
+.spinner .bounce {
+  display: inline-block;
+  width: 15px;
+  height: 15px;
+  margin: 0 0.1rem;
+  border-radius: 100px;
+  background-color: #2c3e50;
+  animation: upDown 1s ease-in-out infinite;
+}
+.spinner .bounce2 {
+  animation-delay: 0.2s;
+}
+.spinner .bounce3 {
+  animation-delay: 0.4s;
+}
+
+.fade-enter-active {
+  animation: fade 0.15s ease;
+}
+.fade-leave-active {
+  animation: fade 0.15s ease reverse;
+}
+
+@keyframes upDown {
+  0% {
+    transform: scale(0.8);
+  }
+  50% {
+    transform: scale(1.3);
+  }
+  100% {
+    transform: scale(0.8);
+  }
+}
+@keyframes fade {
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
+}
 </style>
